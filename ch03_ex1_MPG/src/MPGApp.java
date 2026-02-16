894a3b171296358f99efade7d6cfbbcb9096b909
briatkins4 <batkins4@smartweb.augustatech.edu>
2026-02-16 18:38:01 -0500

    used math class for rounding

--- ch03_ex1_MPG/src/MPGApp.java
+++ ch03_ex1_MPG/src/MPGApp.java
@@ -20,7 +20,7 @@ public class MPGApp {
             double gallons = Double.parseDouble(input);
             
             double mpg = miles/gallons;
-            System.out.println("Miles per gallon is " + mpg + ".");
+            System.out.println("Miles per gallon is " + (double) Math.round(mpg * 100) / 100 + ".");
             System.out.println(); 
             
             System.out.print("Calculate another MPG? (y/n): ");
